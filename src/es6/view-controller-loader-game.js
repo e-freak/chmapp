2225cc0e00d3b9c6c38d2b189a0b8b2630b35b5d
kazama <Masahiro.Kazama@jrits.ricoh.co.jp>
2017-08-03 13:04:29 +0900

    暫定的に牌画像を並べた

--- src/es6/view-controller-loader-game.js
+++ src/es6/view-controller-loader-game.js
@@ -1,12 +1,6 @@
 import ExitButton from '../js/view-controller-game_exit-button';
-// import ConnectLocalRadio from '../js/view-controller-index_connect-local-radio';
-// import ConnectRemoteRadio from '../js/view-controller-index_connect-remote-radio';
 
 global.window.addEventListener('DOMContentLoaded', () => {
     global.objExitButton = new ExitButton(global.document);
     global.objExitButton.initialize();
-    // global.objConnectLocalRadio = new ConnectLocalRadio(global.document);
-    // global.objConnectLocalRadio.initialize();
-    // global.objConnectRemoteRadio = new ConnectRemoteRadio(global.document);
-    // global.objConnectRemoteRadio.initialize();
 }, false);
