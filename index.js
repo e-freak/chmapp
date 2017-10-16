c24ab55623518ed98ae2dbcc8bf341f13b1d12d2
kazama <Masahiro.Kazama@jrits.ricoh.co.jp>
2017-10-16 17:13:05 +0900

    ウィンドウサイズに合わせてゲーム画面のUIサイズを変更するよう修正

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const { app, BrowserWindow } = require('electron')
 let mainWindow;
 
 // 全てのウィンドウが閉じたら終了
-app.on('window-all-closed', function() {
+app.on('window-all-closed', () => {
   if (process.platform != 'darwin') {
     app.quit();
   }
 });
 
 // Electronの初期化完了後に実行
-app.on('ready', function() {
+app.on('ready', () => {
   // メイン画面の表示
   mainWindow = new BrowserWindow({
     width: 960,
@@ -25,7 +25,15 @@ app.on('ready', function() {
   // mainWindow.webContents.openDevTools();
 
   //ウィンドウが閉じられたらアプリも終了
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', () => {
     mainWindow = null;
   });
+
+  // ウィンドウサイズのアスペクト比固定
+  // mainWindow.on('resize', () => {
+  //   setTimeout(() => {
+  //     let size = mainWindow.getSize();
+  //     mainWindow.setSize(size[0], parseInt(size[0] * 3 / 4));
+  //   }, 10);
+  // });
 });
